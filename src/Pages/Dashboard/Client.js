294923348a2c6b095032f73f146f9b76cb27db8f
dcartist <pbannerman13@gmail.com>
2022-01-23 19:01:57 -0500

    adding telephone

--- src/Pages/Dashboard/Client.js
+++ src/Pages/Dashboard/Client.js
@@ -1,11 +1,37 @@
-import React from 'react';
-
 import { Panel } from 'primereact/panel';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react';
 export default function Client() {
-  return <div>
-      <Panel></Panel>
-      <Panel></Panel>
-      <Panel></Panel>
+    const [data, setData] = useState([]);
+    useEffect(() =>
+    axios.get("https://ancient-fjord-10674.herokuapp.com/api/client/id/full/15")
+    .then(res => setData(res.data))
+    // .then(res => console.log(res.data))
+  )
 
-  </div>;
+  function formatPhoneNumber(phoneNumberString) {
+    var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+    var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      var intlCode = (match[1] ? '+1 ' : '');
+      return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+    }
+    return null;
+  }
+
+  if (data.length === 0){
+    return <div>
+    <Panel> Loading... </Panel>
+
+</div>;
+  } else {
+      console.log(data)
+    return <div>
+    <Panel>{data.ownFirstName} {data.ownLastName}</Panel>
+    <Panel>{formatPhoneNumber(data.owner_sphone__)}</Panel>
+    <Panel></Panel>
+
+</div>;
+  }
+  
 }
---
+++ src/components/Text/Telephone.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Telephone(props) {
+    function formatPhoneNumber(phoneNumberString) {
+        var cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+        var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+        if (match) {
+          var intlCode = (match[1] ? '+1 ' : '');
+          return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+        }
+        return null;
+      }
+    
+  return <div>{formatPhoneNumber(props)}</div>;
+}
